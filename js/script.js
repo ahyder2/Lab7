0a2ce05b14ca453b3f4e31170a374668f2d65e77
Asim Hyder <ahyder2@hawk.iit.edu>
2019-11-21 20:42:05 -0600

    Moved variables

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-var input = document.getElementById('entryinput');
-var out = document.getElementById('textoutput');
-var entry = document.getElementById('entrybutton');
 function init(){
 //add your javascrip between these two lines of code
+var entry = document.getElementById('entrybutton');
 entry.addEventListener('click', AlertMe);
 entry.addEventListener('click', ShowResultsHere);
 }
 
 function AlertMe(){
+	var input = document.getElementById('entryinput');
 	alert('Asim Hyder: ' + input.value);
 }
 function ShowResultsHere(){
+	var input = document.getElementById('entryinput');
+	var out = document.getElementById('textoutput');
 	out.innerHTML = input.value;
 }
 
