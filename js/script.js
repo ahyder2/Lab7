d6c5188a86fdb4e2012cb903d64d7c6076abdec6
Asim Hyder <ahyder2@hawk.iit.edu>
2019-11-21 20:00:15 -0600

    First attempt to complete lab. Should be complete

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 var in;
 var out;
+var entry;
 function init(){
 //add your javascrip between these two lines of code
 
-
-
-
-
-
-
+in = document.getElementById('entryinput');
+out = document.getElementById('textoutput');
+entry = document.getElementById('entrybutton');
+function AlertMe(){
+	alert('Asim Hyder: ' + in.value)
+}
+function ShowResultsHere(){
+	out.innerHTML = in.value;
+}
+entry.addEventListener('click', AlertMe);
+entry.addEventListener('click', ShowResultsHere);
 
 window.addEventListener('load', init);
