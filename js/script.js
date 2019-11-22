0423d00afbe8a4a65379e7f87c0fcdaba9b8f503
Asim Hyder <ahyder2@hawk.iit.edu>
2019-11-21 20:50:41 -0600

    Changing some logical placement to see if it will work

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 function init(){
 //add your javascrip between these two lines of code
-var entry = document.getElementById('entrybutton');
-entry.addEventListener('click', AlertMe);
-entry.addEventListener('click', ShowResultsHere);
-}
-
-function AlertMe(){
-	var input = document.getElementById('entryinput');
-	alert('Asim Hyder: ' + input.value);
-}
-function ShowResultsHere(){
 	var input = document.getElementById('entryinput');
 	var out = document.getElementById('textoutput');
-	out.innerHTML = input.value;
+	var entry = document.getElementById('entrybutton');
+	function AlertMe(){
+		alert('Asim Hyder: ' + input.value);
+	}
+	function ShowResultsHere(){
+		out.innerHTML = input.value;
+	}
+	entry.addEventListener('click', AlertMe);
+	entry.addEventListener('click', ShowResultsHere);
 }
 
 window.addEventListener('load', init);
