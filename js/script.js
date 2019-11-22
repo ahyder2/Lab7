835e4ef06d1e3e8fa614e9359a5371afc470c534
Asim Hyder <ahyder2@hawk.iit.edu>
2019-11-21 20:14:57 -0600

    Trying to fix. Also removed duplicate file after renaming

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
-var in;
-var out;
-var entry;
 function init(){
 //add your javascrip between these two lines of code
 
-in = document.getElementById('entryinput');
-out = document.getElementById('textoutput');
-entry = document.getElementById('entrybutton');
+var in = document.getElementById('entryinput');
+var out = document.getElementById('textoutput');
+var entry = document.getElementById('entrybutton');
 entry.addEventListener('click', AlertMe);
 entry.addEventListener('click', ShowResultsHere);
 }
